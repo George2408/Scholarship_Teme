9a721e7bf70256518cc4e13dea2c1be60dd02736
gEORGICA2023 <93447191+gEORGICA2023@users.noreply.github.com>
2022-03-16 18:25:26 +0200

    Tema 9

---
+++ Tema_9 Partea de .NET/NotesAPI/NotesAPI/Controllers/NotesController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NotesAPI.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class NotesController : ControllerBase
+    {
+        /// <summary>
+        /// Get all notes
+        /// </summary>
+        /// <returns></returns>
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Note controller works");
+        }
+    }
+
+}
+
