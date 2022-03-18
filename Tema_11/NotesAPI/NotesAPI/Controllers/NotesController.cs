954a7d992ceba2f5b902065965164105be59e71c
gEORGICA2023 <93447191+gEORGICA2023@users.noreply.github.com>
2022-03-18 15:07:25 +0200

    Tema 10

---
+++ Tema_11/NotesAPI/NotesAPI/Controllers/NotesController.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NotesAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NotesAPI.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class NotesController : ControllerBase
+    {
+        static List<Note> _notes = new List<Note> { 
+        new Note { Id = Guid.NewGuid(), CategoryId = "1", OwnerId = Guid.NewGuid(), Title = "First Note", Description = "First Note Description" },
+        new Note { Id = Guid.NewGuid(), CategoryId = "1", OwnerId = Guid.NewGuid(), Title = "Second Note", Description = "Second Note Description" },
+        new Note { Id = Guid.NewGuid(), CategoryId = "1", OwnerId = Guid.NewGuid(), Title = "Third Note", Description = "Third Note Description" },
+        new Note { Id = Guid.NewGuid(), CategoryId = "1", OwnerId = Guid.NewGuid(), Title = "Fourth Note", Description = "Fourth Note Description" },
+        new Note { Id = Guid.NewGuid(), CategoryId = "1", OwnerId = Guid.NewGuid(), Title = "Fifth Note", Description = "Fifth Note Description" }
+        };
+
+
+        /// <summary>
+        /// Get all notes
+        /// </summary>
+        /// <returns></returns>
+
+        [HttpGet]
+        public IActionResult GetNotes()
+        {
+            return Ok(_notes);
+        }
+
+        
+        [HttpPost]
+        public IActionResult CreateNotes([FromBody] Note note)
+        {
+           _notes.Add(note);
+            //return Ok(_notes);
+            return CreatedAtRoute("GetNote", new { id= note.Id.ToString() }, note);
+        }
+
+        [HttpGet("Owner/{ownerId}")]
+        public IActionResult GetNotesByOwnerId( string ownerId)
+        {
+            var filtredList = new List<Note>();
+            foreach(var note in _notes)
+            {
+                if(note.OwnerId.ToString().Equals(ownerId))
+                {
+                    filtredList.Add(note);
+                }
+            }
+            return Ok(filtredList);
+        }
+
+        [HttpGet("{id}", Name = "GetNote")]
+        public IActionResult GetNoteById(string id)
+        {
+            var foundNote = new Note();
+            foreach (var note in _notes)
+            {
+                if (note.Id.ToString().Equals(id))
+                {
+                   foundNote = note;
+                }
+            }
+            return Ok(foundNote);
+        }
+
+        //[HttpGet]
+        //public ActionResult<List<Note>> Get() =>
+        //    _notes.Get();
+
+        //[HttpGet("{id}")]
+        //public ActionResult<Note> Get(string id)
+        //{
+        //    var note = _notes.Get(id);
+
+        //    if (note == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    return note;
+        //}
+
+
+    }
+
+
+}
+
---
+++ Tema_11/NotesAPI/NotesAPI/Controllers/OwnerController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NotesAPI.Models;
+using System;
+using System.Collections.Generic;
+
+namespace NotesAPI.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class OwnerController : ControllerBase
+    {
+        public List<Owner> _owner = new List<Owner>{
+            new Owner { Id = Guid.NewGuid(), Name = "Grigore" },
+            new Owner { Id = Guid.NewGuid(), Name = "George" },
+            new Owner { Id = Guid.NewGuid(), Name = "Alexandra" },
+            new Owner { Id = Guid.NewGuid(), Name = "Vasile" },
+        };
+
+        [HttpGet]
+        public IActionResult GetOwner()
+        {
+            return Ok(_owner);
+        }
+
+        [HttpPost]
+        public IActionResult CreateOwner([FromBody] Owner owner)
+        {
+            _owner.Add(owner);
+            return Ok(_owner);
+        }
+    }
+}
